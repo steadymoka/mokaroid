afb2681446d8a78799c1aa74d315a1a406826401
moka-a <aud1220a@gmail.com>
2020-07-31 16:58:02 +0900

    gradle -> gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+buildscript {
+    val kotlin_version: String by extra
+
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.1.0-beta05")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlin_version")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.0")
+        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
+        classpath("com.apollographql.apollo:apollo-gradle-plugin:1.2.2")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven("https://jitpack.io")
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
