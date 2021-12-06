f83ed176f3b3f65a73c3c16f90977c231a8e4037
moka <aud1220a@gmail.com>
2021-12-06 14:05:41 +0900

    fix image thumbnail

--- transcoder/src/main/java/moka/land/transcoder/utils/Thumbnail.kt
+++ transcoder/src/main/java/moka/land/transcoder/utils/Thumbnail.kt
@@ -2,14 +2,9 @@ package moka.land.transcoder.utils
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.ImageDecoder
 import android.media.MediaMetadataRetriever
 import android.net.Uri
-import android.os.Build
-import android.provider.MediaStore
 import android.util.Base64
-import moka.land.base.log
-import moka.land.fileutil._FileUtil.Companion.context
 import moka.land.fileutil.uri2path
 import java.io.ByteArrayOutputStream
 import java.io.IOException
@@ -17,13 +12,12 @@ import java.io.IOException
 
 object Thumbnail {
 
-    fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+    private fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
         // Raw height and width of image
         val (height: Int, width: Int) = options.run { outHeight to outWidth }
         var inSampleSize = 1
 
         if (height > reqHeight || width > reqWidth) {
-
             val halfHeight: Int = height / 2
             val halfWidth: Int = width / 2
 
@@ -33,37 +27,30 @@ object Thumbnail {
                 inSampleSize *= 2
             }
         }
-
         return inSampleSize
     }
 
-    fun imageUriToBitmap(uri: Uri): Bitmap {
+    fun imageUriToBitmap(uri: Uri, reqWidth: Int = 200, reqHeight: Int = 200): Bitmap {
         var bitmap: Bitmap? = null
 
         try {
-            bitmap = if (uri.scheme == "file") {
-                BitmapFactory.Options().run {
-                    inJustDecodeBounds = true
-                    BitmapFactory.decodeFile(uri.path, this)
-                    inSampleSize = calculateInSampleSize(this, 100, 100)
-                    inJustDecodeBounds = false
+            val path = uri2path(uri)
+            bitmap = BitmapFactory.Options().run {
+                inJustDecodeBounds = true
+                BitmapFactory.decodeFile(path, this)
+                inSampleSize = calculateInSampleSize(this, reqWidth, reqHeight)
+                inJustDecodeBounds = false
 
-                    BitmapFactory.decodeFile(uri.path, this)
-                }
-            } else {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    ImageDecoder.decodeBitmap(ImageDecoder.createSource(context.contentResolver, uri))
-                } else {
-                    MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
-                }
+                BitmapFactory.decodeFile(path, this)
             }
+
         } catch (e: IOException) {
             e.printStackTrace()
         }
         return bitmap!!
     }
 
-    fun bitmapToBase64(bitmap: Bitmap, quality: Int = 60): String {
+    fun bitmapToBase64(bitmap: Bitmap, quality: Int = 100): String {
         val byteArrayOutputStream = ByteArrayOutputStream()
         bitmap.compress(Bitmap.CompressFormat.PNG, quality, byteArrayOutputStream)
         val byteArray = byteArrayOutputStream.toByteArray()
