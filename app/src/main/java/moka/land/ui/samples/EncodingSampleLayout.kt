06d07bdc45970a93ab270ff946f62a82afd8e80b
moka <aud1220a@gmail.com>
2021-12-06 11:39:15 +0900

    fix image thumbnail

--- app/src/main/java/moka/land/ui/samples/EncodingSampleLayout.kt
+++ app/src/main/java/moka/land/ui/samples/EncodingSampleLayout.kt
@@ -1,41 +1,24 @@
 package moka.land.ui.samples
 
-import android.graphics.Bitmap
-import android.media.MediaMetadataRetriever
-import android.media.ThumbnailUtils
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.os.ParcelFileDescriptor
-import android.provider.MediaStore
-import android.util.Base64
-import android.util.Log
-import android.util.Size
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.api.FileUpload
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import kotlinx.coroutines.launch
-import moka.land.R
 import moka.land.base.log
 import moka.land.databinding.LayoutEncodingSampleBinding
 import moka.land.imagehelper.picker.builder.ImagePicker
 import moka.land.imagehelper.picker.type.MediaType
 import moka.land.imagehelper.viewer.ImageViewer
-import moka.land.modules.awaitEnqueue
 import moka.land.transcoder.MediaTranscoder
 import moka.land.transcoder.format.MediaFormatStrategyPresets
 import moka.land.transcoder.utils.Thumbnail
 import moka.land.util.load
 import org.koin.android.ext.android.inject
-import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileNotFoundException
 import java.util.concurrent.Future
@@ -67,16 +50,17 @@ class EncodingSampleLayout : Fragment() {
             ImagePicker
                 .with(this)
                 .setConfig {
-                    mediaType = MediaType.IMAGE_VIDEO
+                    mediaType = MediaType.VIDEO_ONLY
                     implicit = true
                 }
                 .showSingle { uri ->
-                    val bitmap = Thumbnail.createBitmapImageThumbnail(uri)
-                    val base64 = Thumbnail.bitmapToBase64(bitmap)
-                    log(base64)
-                    _view.imageViewTarget.setImageBitmap(bitmap)
-//                    Thumbnail.createBase64Thumbnail(uri)
-//                    transcode(uri)
+//                    val bitmap = Thumbnail.imageUriToBitmap(uri)
+//                    val base64 = Thumbnail.bitmapToBase64(bitmap)
+//                    _view.imageViewTarget.setImageBitmap(bitmap)
+
+                    val bit = Thumbnail.videoUriToBitmap(uri)
+                    _view.imageViewTarget.setImageBitmap(bit)
+                    transcode(uri)
                 }
         }
 
--- transcoder/src/main/java/moka/land/transcoder/utils/Thumbnail.kt
+++ transcoder/src/main/java/moka/land/transcoder/utils/Thumbnail.kt
@@ -1,12 +1,14 @@
 package moka.land.transcoder.utils
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
 import android.media.MediaMetadataRetriever
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
 import android.util.Base64
+import moka.land.base.log
 import moka.land.fileutil._FileUtil.Companion.context
 import moka.land.fileutil.uri2path
 import java.io.ByteArrayOutputStream
@@ -15,13 +17,18 @@ import java.io.IOException
 
 object Thumbnail {
 
-    fun createBitmapImageThumbnail(uri: Uri): Bitmap {
+    fun imageUriToBitmap(uri: Uri): Bitmap {
         var bitmap: Bitmap? = null
+
         try {
-            bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                ImageDecoder.decodeBitmap(ImageDecoder.createSource(context.contentResolver, uri))
+            bitmap = if (uri.scheme == "file") {
+                BitmapFactory.decodeFile(uri.path)
             } else {
-                MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    ImageDecoder.decodeBitmap(ImageDecoder.createSource(context.contentResolver, uri))
+                } else {
+                    MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
+                }
             }
         } catch (e: IOException) {
             e.printStackTrace()
@@ -29,14 +36,14 @@ object Thumbnail {
         return bitmap!!
     }
 
-    fun bitmapToBase64(bitmap: Bitmap, quality: Int = 100): String {
+    fun bitmapToBase64(bitmap: Bitmap, quality: Int = 60): String {
         val byteArrayOutputStream = ByteArrayOutputStream()
         bitmap.compress(Bitmap.CompressFormat.PNG, quality, byteArrayOutputStream)
         val byteArray = byteArrayOutputStream.toByteArray()
         return Base64.encodeToString(byteArray, Base64.DEFAULT)
     }
 
-    fun createBitmapThumbnail(uri: Uri): Bitmap {
+    fun videoUriToBitmap(uri: Uri): Bitmap {
         val path = uri2path(uri)
 
         val retriever = MediaMetadataRetriever()
@@ -48,9 +55,8 @@ object Thumbnail {
         )
     }
 
-    fun createBase64Thumbnail(uri: Uri): String {
-        return bitmapToBase64(createBitmapThumbnail(uri))
+    fun videoUriToBase64(uri: Uri): String {
+        return bitmapToBase64(videoUriToBitmap(uri))
     }
-
 
 }
